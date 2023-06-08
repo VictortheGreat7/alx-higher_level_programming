3657a9d12b72b306b435009fabe945545f6d533e
Great-Victor Anjorin <greatvictor.anjorin@gmail.com>
2023-06-08 14:51:27 +0000

    Python - import & modules: Task 2 - Check 1

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python3
-from sys import argv
 
-if len(argv) == 1:
-    print("0 arguments.")
-elif len(argv) == 2:
-    print("1 argument:")
-    print("1: {}".format(argv[1]))
-else:
-    print("{} arguments:".format(len(argv) - 1))
-    for i, arg in enumerate(argv[1:], start=1):
-        print("{}: {}".format(i, arg))
+if __name__ == "__main__":
+    from sys import argv
+
+    if len(argv) == 1:
+        print("0 arguments.")
+    elif len(argv) == 2:
+        print("1 argument:")
+        print("1: {}".format(argv[1]))
+    else:
+        print("{} arguments:".format(len(argv) - 1))
+        for i, arg in enumerate(argv[1:], start=1):
+            print("{}: {}".format(i, arg))
