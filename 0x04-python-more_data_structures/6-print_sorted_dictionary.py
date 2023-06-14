161e8b361f77ce79c796db1ba93c4b8de6b008da
Great-Victor Anjorin <greatvictor.anjorin@gmail.com>
2023-06-14 15:57:21 +0000

    Task 6 and 7 Check 2

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,13 +1,5 @@
 #!/usr/bin/python3
 
 def print_sorted_dictionary(a_dictionary):
-    def recursive_sort(dictionary):
-        sorted_keys = sorted(dictionary.keys())
-        for key in sorted_keys:
-            value = dictionary[key]
-            if isinstance(value, dict):
-                print(key, ":")
-                recursive_sort(value)
-            else:
-                print(key, ":", value)
-    recursive_sort(a_dictionary)
+    for key in sorted(a_dictionary.keys()):
+        print("{:s}: {}".format(key, a_dictionary[key]))
