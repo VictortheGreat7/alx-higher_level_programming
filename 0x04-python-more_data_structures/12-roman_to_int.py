726ea7103a62ab5c2c6d0037aae3bfa0ccc2b7af
Great-Victor Anjorin <greatvictor.anjorin@gmail.com>
2023-06-14 17:09:19 +0000

    Python - More Data Structures: Set, Dictionary: Task 12 - Check 1

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+
+def roman_to_int(roman_string):
+    if roman_string is None or not isinstance(roman_string, str):
+        return (0)
+    roman_numerals = {
+            'I': 1,
+            'V': 5,
+            'X': 10,
+            'L': 50,
+            'C': 100,
+            'D': 500,
+            'M': 1000
+            }
+    int_value = 0
+    prev_value = 0
+
+    for numeral in reversed(roman_string):
+        value = roman_numerals.get(numeral, 0)
+        if value >= prev_value:
+            int_value += value
+        else:
+            int_value -= value
+        prev_value = value
+    return (int_value)
