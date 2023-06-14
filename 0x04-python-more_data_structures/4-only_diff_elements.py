29dfdb53c2899166d643690186d8876b65eed46e
Great-Victor Anjorin <greatvictor.anjorin@gmail.com>
2023-06-14 15:02:54 +0000

    Python - More Data Structures: Set, Dictionary: Task 4 - Check 1

---
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+def only_diff_elements(set_1, set_2):
+    unique_set = set()
+
+    for i in set_1:
+        if i not in set_2:
+            unique_set.add(i)
+
+    for i in set_2:
+        if i not in set_1:
+            unique_set.add(i)
+
+    return (unique_set)
