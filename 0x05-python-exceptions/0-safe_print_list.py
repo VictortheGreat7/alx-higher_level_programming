07b08a93ddd4c04e0b500243173eee1244cdab9b
Great-Victor Anjorin <greatvictor.anjorin@gmail.com>
2023-06-26 10:07:45 +0000

    Python - Exceptions: Task 0 - Check 1

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+def safe_print_list(my_list=[], x=0):
+    try:
+        count = 0
+        for item in my_list:
+            if count < x:
+                print(item, end="")
+                count += 1
+        print()
+        return (count)
+    except TypeError:
+        print("Unknown error occured while printing the list.")
+        return (count)
