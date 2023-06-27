86e5230a66cc1242b43f067570efee0dc5f5e060
Great-Victor Anjorin <greatvictor.anjorin@gmail.com>
2023-06-27 15:12:15 +0000

    Python - Classes and Objects: Task 1 - Check 1

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,1 +1,14 @@
 #!/usr/bin/python3
+"""Square Class"""
+
+
+class Square:
+    """A class Square with a private instance attribute: size"""
+
+    def __init__(self, size):
+        """Initializing a square instance
+
+        Args:
+            size (int): The size of the square
+        """
+        self.__size = size
