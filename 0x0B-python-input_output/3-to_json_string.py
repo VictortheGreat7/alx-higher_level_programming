1a87a00091aae69eed34faf480214b898c5b684d
Great-Victor Anjorin <greatvictor.anjorin@gmail.com>
2023-07-13 14:11:26 +0000

    0x0B. Python - Input/Output: Task 3 - Check 1

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+Module for the to_json_string function
+"""
+
+
+import json
+
+
+def to_json_string(my_obj):
+    """Returns the JSON representation of an object (string)
+
+    Args:
+        my_obj: The object string
+
+    Returns:
+        The JSON representation of the object string
+    """
+    return json.dumps(my_obj)
